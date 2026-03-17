1718540bc855ea6b494aaacdd109da5860bd2c3e
David-Fruehwirt <d.fruehwirt@students.htl-leonding.ac.at>
2026-03-17 13:36:05 +0100

    try

---
+++ src/frontend/src/app/components/account/account.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  imports: [],
+  templateUrl: './account.html',
+  styleUrl: '../../css/settings.css',
+})
+export class Account {
+  saveChanges(): void {
+
+  }
+
+  updatePassword(): void {
+
+  }
+}
