48a4caa87079dea7688b11679cebe35ccd98b289
Timon-Brindl <t.brindl@students.htl-leonding.ac.at>
2026-04-16 21:45:08 +0200

    implemented a bit of Marketplace Logic

--- src/middleground/Marketplace.ts
+++ src/middleground/Marketplace.ts
@@ -3,6 +3,10 @@ import {Listing, ListingRow} from "../shared/model";
 export class MarketPlace {
     listings: ListingRow[] = new Array<ListingRow>();
 
+    constructor() {
+        this.listings = this.getListingsFromDatabase();
+    }
+
     public createListing(sellerId:number, stoveId:number, price:number ): void {
         this.listings.push({
             listedAt: new Date(Date.now()),
@@ -12,6 +16,33 @@ export class MarketPlace {
             stoveId: stoveId,
             price:price
         })
+        //TODO: Add listing to database
     }
+
+    private getListingsFromDatabase() : ListingRow[] {
+        //TODO: Get From Database
+        return [];
+    }
+
+    public executePurchase(buyerId: number, listingId: number): void {
+        const listing = this.listings.find(value => value.listingId == listingId);
+        if (!listing || listing.status != "active") {
+            return;
+        }
+        listing.status = 'sold';
+        //TODO: add Item to buyer database && remove Item from listing Database
+        //TODO: Add to sales Statistics
+    }
+
+    public cancel(listingId:number, currentUserId: number): void {
+        const listing = this.listings.find(value => value.listingId == listingId);
+        if (!listing || currentUserId != listing.sellerId) {
+            return;
+        }
+        listing.status = 'cancelled';
+        //TODO: Update Database
+    }
+
+
 
 }
